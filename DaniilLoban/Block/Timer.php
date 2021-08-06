bb4c461300d8fb3074a059f1cc0950abfdd85ec2
daniil-loban <dungeon_lda@mail.ru>
2021-08-06 19:18:26 +0300

    fix: format 4 spaces

--- DaniilLoban/Block/Timer.php
+++ DaniilLoban/Block/Timer.php
@@ -3,19 +3,19 @@ namespace Amasty\DaniilLoban\Block;
 
 class Timer extends \Magento\Framework\View\Element\Template
 {
-  /**
-   * @param \Magento\Framework\View\Element\Template\Context $context
-   * @param array $data
-   */
-  public function __construct(
-      \Magento\Framework\View\Element\Template\Context $context,
-      array $data = []
-  ) {
-      parent::__construct($context, $data);
-  }
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
 
-  public function getText()
-  {
-    return "There will be a timer soon, next time! The 🕑ne is late. :)";
-  }
+    public function getText()
+    {
+        return "There will be a timer soon, next time! The 🕑ne is late. :)";
+    }
 }
--- DaniilLoban/Controller/Index/Index.php
+++ DaniilLoban/Controller/Index/Index.php
@@ -6,8 +6,8 @@ use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
-  public function execute()
-  {
-    return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
-  }
+    public function execute()
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
 }
