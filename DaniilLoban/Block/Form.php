40f5e442031ba260664a65ae4e6c7b402c3cd899
daniil-loban <dungeon_lda@mail.ru>
2021-08-10 19:20:03 +0300

    feat: add settings for the module

--- DaniilLoban/Block/Form.php
+++ DaniilLoban/Block/Form.php
@@ -1,17 +1,34 @@
 <?php
+
 namespace Amasty\DaniilLoban\Block;
 
-class Form extends \Magento\Framework\View\Element\Template
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\View\Element\Template;
+
+class Form extends Template
 {
     /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param array $data
+     * @var ScopeConfigInterface
      */
+    private $scopeConfig;
+
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
+        Context $context,
+        ScopeConfigInterface $scopeConfig,
         array $data = []
     ) {
         parent::__construct($context, $data);
+        $this->scopeConfig = $scopeConfig;
     }
-    
+
+    public function isShowQty()
+    {
+        return $this->scopeConfig->getValue('daniilloban_config/general/is_show_qty');
+    }
+
+    public function getDefaultQty()
+    {
+        return (int)$this->scopeConfig->getValue('daniilloban_config/general/default_qty');
+    }
 }
