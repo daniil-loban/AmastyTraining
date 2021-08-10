019c3ba5812c470a0dc57527d0477aeb71b37d61
daniil-loban <dungeon_lda@mail.ru>
2021-08-10 23:12:08 +0300

    feat: add a custom config provider

---
+++ DaniilLoban/Model/ConfigProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Amasty\DaniilLoban\Model;
+
+use \Amasty\DaniilLoban\Model\ConfigProviderAbstract;
+use Magento\Checkout\Model\Session;
+
+class ConfigProvider extends ConfigProviderAbstract
+{
+    /**
+     * @var Session
+     */
+    private $session;
+
+    public function __construct(
+        //ScopeConfigInterface $scopeConfig
+        Session $messageManager
+    ) {
+        $this->session = $messageManager;
+    }
+
+    public function getConfig()
+    {
+        $storeId = $this->session->getStoreId();
+        return [
+            'enabled' => $this->getIsEnabled($storeId),
+            'is_show_qty' => $this->getIsShowQtyField($storeId),
+            'default_qty' => $this->getQtyFieldDefaultValue($storeId)
+        ];
+    }
+
+    public function getIsEnabled($storeId)
+    {
+        return (bool)$this->GetValue('/general/enabled', $storeId);
+    }
+
+    public function getIsShowQtyField($storeId)
+    {
+        return (bool)$this->GetValue('/general/is_show_qty', $storeId);
+    }
+
+    public function getQtyFieldDefaultValue($storeId)
+    {
+        return (int)$this->GetValue('/general/default_qty', $storeId);
+    }
+}
